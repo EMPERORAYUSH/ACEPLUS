90c6e06aecadc5c6a5bfb64c758e1f4d743af451
EMPERORAYUSH <ayusha17022010@gmail.com>
2025-04-26 13:45:53 +0530

    Added lesson names

--- backend/utils/lesson_utils.py
+++ backend/utils/lesson_utils.py
@@ -2,18 +2,20 @@ import os
 
 def lesson2filepath(subject, lesson, class10=False):
     subject_lower = subject.lower()
-    lesson_number = lesson.split()[-1]
+    if subject != "SS":
+        lesson_number = lesson[0]
     base_folder = "lessons10" if class10 else "lessons"
     script_dir = os.path.dirname(os.path.abspath(__file__))
     data_path = os.path.join(script_dir, "..", "data")
 
     if subject == "SS":
-        prefix = lesson.split(":")[0].lower()
+        lesson_number=lesson[1]
+        prefix = lesson[0]
         path = os.path.join(
             data_path,
             base_folder,
             subject_lower,
-            f"{prefix}.{lesson_number.split('-')[1]}.json",
+            f"{prefix}.{lesson_number}.json",
         )
     elif subject == "Science":
         path = os.path.join(
