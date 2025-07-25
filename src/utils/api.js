31394278f040495ebf86512e36ff7b3aa3d9edc5
EMPERORAYUSH <ayusha17022010@gmail.com>
2025-07-26 00:05:44 +0530

    Fixed api issue with vite

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,6 @@
 // API Base URL configuration
-const DEV_API_URL = 'http://127.0.0.1:9027';
-const PROD_API_URL = 'https://aceplus.vercel.app';
 
-// For debugging
-console.log('Current NODE_ENV:', import.meta.env.NODE_ENV);
-console.log('Current REACT_APP_ENV:', import.meta.env.VITE_APP_ENV);
-
-export const API_BASE_URL = import.meta.env.NODE_ENV === 'production' ? PROD_API_URL : DEV_API_URL;
+export const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 // Default headers that should be included in most requests
 const getDefaultHeaders = () => {
